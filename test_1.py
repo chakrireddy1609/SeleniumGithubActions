03577623e579727b3f7cdd9767d3bf25ed36badc
chakrireddy1609 <chakrireddy1609@gmail.com>
2023-01-19 11:57:54 +0530

    actions changes

--- test_1.py
+++ test_1.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium.webdriver.common.by import By
 
@@ -5,10 +7,12 @@ from selenium.webdriver.common.by import By
 class Test_Sample1():
     def test_title(self):
         self.driver.get("https://accounts.staging.joveo.com/")
+        time.sleep(5)
         assert self.driver.title == "Joveo Account"
 
     def test_text(self):
         self.driver.get("https://accounts.staging.joveo.com/")
+        time.sleep(5)
         text_shown = self.driver.find_element(By.XPATH, "//h1[@class='mt-4']").text
         assert text_shown == "Log in to Joveo Account"
 
